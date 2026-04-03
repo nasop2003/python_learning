922c9a877dda3c457c7442d397d64d3e429a9c91
nasop2003 <ahsommi@gmail.com>
2026-04-03 17:23:44 +0900

    delete files

--- 初心者/標準ライブラリ/Library_Datetime.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-now = datetime.datetime.now()
-
-print("現在時刻:", now)
-print("今日の日付:", now.date())
-print("現在時刻:", now.time())
--- 初心者/標準ライブラリ/Random+Datetime.py
+++
@@ -1,8 +0,0 @@
-import random
-import datetime
-
-now = datetime.datetime.now()
-number = random.randint(1, 100)
-
-print("現在時刻:", now)
-print("今日のラッキーナンバー:", number)
